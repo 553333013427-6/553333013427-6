7948dcbe37130d3ffb635116334eda200b7e6268
553333013427-6 <puyunee55@hotmail.com>
2014-04-03 21:08:03 +0700

    Update Test1.java

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -3,7 +3,7 @@ package test1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("PUTCHARIN PRAJOUBTHAEN ECP3Q 553333013427-6");
 		
 
 	}
